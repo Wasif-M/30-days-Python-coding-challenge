eb4098bda6b51e46d4fe40f5878efbedd888e680
WASIF-M <wasif5690@gmail.com>
2023-03-20 22:35:15 +0500

    commit

---
+++ operators.py
@@ -0,0 +1,16 @@
+#Arithmetic Operators
+
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+Raining =True
+temperature = 30
+out = not Raining and temperature > 20
+print(out)
