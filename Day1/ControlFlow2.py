b3fca536f743efbb369678d291ccb1f8421be9e8
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2023-03-21 17:23:40 +0500

    Commit

---
+++ Day1/ControlFlow2.py
@@ -0,0 +1,7 @@
+high_income=False
+good_credit=True
+student=False
+if (high_income and good_credit) or not student:
+    print("Eligible")
+else:
+    print("Not Eligible")
---
+++ Day1/variables.py
@@ -0,0 +1,9 @@
+#Variables in Python
+number = 10
+average = 2.0
+greeting = "Hello"
+will_go = True
+print(number)
+print(average)
+print(greeting)
+print(will_go)
