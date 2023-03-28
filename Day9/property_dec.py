d7b816f9e334d81d838fe7ea9d0ba4bacef8144d
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2023-03-28 23:58:05 +0500

    Day9

---
+++ Day9/property_dec.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__(self,first_name,last_name,age,salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.salary = salary
+    def fullname(self):
+        return f"{self.first_name} {self.last_name}"
+    def email(self):
+        return f"{}.{}@gmail.com".format(self.first_name,self.last_name)
+    
+emp_1=  Employee("John","Doe",25,4500)
